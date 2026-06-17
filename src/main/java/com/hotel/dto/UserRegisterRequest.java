3a13dd0d5e1bb3dd1560c85e6f07242ff4998b8f
hy1589389 <2049899625@qq.com>
2026-06-17 19:53:55 +0800

    添加用户注册请求DTO

---
+++ src/main/java/com/hotel/dto/UserRegisterRequest.java
@@ -0,0 +1,53 @@
+package com.hotel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/**
+ * 用户注册请求DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRegisterRequest {
+
+    /**
+     * 用户名
+     */
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    @NotBlank(message = "真实姓名不能为空")
+    private String realName;
+
+    /**
+     * 邮箱
+     */
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+
+    /**
+     * 手机号
+     */
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    private String phone;
+
+}
