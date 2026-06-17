5abf873fa05a70ab89c142554066d75ae4457b62
hy1589389 <2049899625@qq.com>
2026-06-17 19:53:24 +0800

    添加用户登录请求DTO

---
+++ src/main/java/com/hotel/dto/UserLoginRequest.java
@@ -0,0 +1,31 @@
+package com.hotel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 用户登录请求DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserLoginRequest {
+
+    /**
+     * 用户名
+     */
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+}
