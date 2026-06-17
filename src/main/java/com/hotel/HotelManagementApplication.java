78c8ca75c780e50dd82ccc232a55c09662bb8ad1
hy1589389 <2049899625@qq.com>
2026-06-17 19:46:51 +0800

    添加应用启动类

---
+++ src/main/java/com/hotel/HotelManagementApplication.java
@@ -0,0 +1,16 @@
+package com.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 酒店民宿管理系统启动类
+ */
+@SpringBootApplication
+public class HotelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementApplication.class, args);
+    }
+
+}
